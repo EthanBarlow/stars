32113c276c093a15455e8ce4aee5faa0be1f51bb
Ethan Barlow <ethanbarlow07@gmail.com>
2021-03-29 23:36:16 -0400

    Base UI design

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -9,19 +9,21 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+const _radius = 50.0;
+const _dateFontSize = 30.0;
+const _explanationFontSize = 16.0;
+const _textInset = 20.0;
+
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String apiKey = "api";
+  String explanation =
+      "Who knows what evil lurks in the eyes of galaxies? The Hubble knows -- or in the case of spiral galaxy M64 -- is helping to find out. Messier 64, also known as the Evil Eye or Sleeping Beauty Galaxy, may seem to have evil in its eye because all of its stars rotate in the same direction as the interstellar gas in the galaxy's central region, but in the opposite direction in the outer regions. Captured here in great detail by the Earth-orbiting Hubble Space Telescope, enormous dust clouds obscure the near-side of M64's central region, which are laced with the telltale reddish glow of hydrogen associated with star formation.  M64 lies about 17 million light years away, meaning that the light we see from it today left when the last common ancestor between humans and chimpanzees roamed the Earth.  The dusty eye and bizarre rotation are likely the result of a billion-year-old merger of two different galaxies.";
+  final imageUrl =
+      'https://apod.nasa.gov/apod/image/2103/M8_HLA_F125wF160w_Nachman.jpg';
   void main() async {
     await DotEnv.load();
     apiKey = DotEnv.env['API_KEY'];
     print(apiKey);
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
   }
 
   @override
@@ -29,30 +31,89 @@ class _MyHomePageState extends State<MyHomePage> {
     main();
     return Scaffold(
       appBar: AppBar(
-        
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              print('refreshing');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              print('sharing');
+            },
+          ),
+        ],
       ),
       body: Center(
-       
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Image.network("https://apod.nasa.gov/apod/image/2102/PIA24333_fig1.jpg"),
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(imageUrl),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(_radius),
+                  bottomRight: Radius.circular(_radius),
+                ),
+                color: Colors.blue,
+                boxShadow: [
+                  const BoxShadow(
+                    color: Colors.black,
+                    offset: Offset(0, 10),
+                    blurRadius: 20,
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(
+                  horizontal: _textInset,
+                  vertical: _textInset * 1.5,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Title of photo',
+                      style: TextStyle(
+                        fontSize: _dateFontSize,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                      width: double.infinity,
+                    ),
+                    Text(
+                      'March 29, 2021',
+                      style: TextStyle(fontSize: _dateFontSize / 1.2),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                      width: double.infinity,
+                    ),
+                    Text(
+                      explanation,
+                      style: TextStyle(
+                        fontSize: _explanationFontSize,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
